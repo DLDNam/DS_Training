b2807f529ab7b8d581cefa569e7bd92399d8aa54
Duc <dinhleducnam2003@gmail.com>
2023-11-12 10:35:26 +0700

    Update

--- Ex2.py
+++ Ex2.py
@@ -1,4 +1,4 @@
-list1 = [1, 2, 3, 2, 4, 3, 5, 6, 4]
+list1 = [int(i) for i in input().split()]
 list2 = []
 for item in list1:
     if item not in list2:
--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-n = int(input())
-list = []
-for i in range(n):
-    list.append(input())
+list = [int(i) for i in input().split()]
+
 print(list)
-    
