69cbdde4ba0786e3f0ce76eb493dbc1493b1ee7a
Duc <dinhleducnam2003@gmail.com>
2023-11-14 14:31:39 +0700

    Update

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-list = [int(i) for i in input().split()]
-
-print(list)
+list1 = set(int(i) for i in input().split() )
+list1 = list(list1)
+list1.sort(reverse = True)
+try:
+    print(list1[1])
+except:
+    print("NULL")
